a4edfeefdf28f1524a4aba1239bd23ed7772c848
Matthew Amador <164225271+42Starship@users.noreply.github.com>
2025-04-12 09:06:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
+// 👀 This code makes the mobile navigation menu (the one with the three bars—also called a "hamburger menu"🍔) work.
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+const menu = document.querySelector('#mobile-menu'); //This grabs the menu button (little three-bar icon) 
+//& it looking for element with the ID mobile menu
+const menuLinks = document.querySelector('.navbar__menu'); //This grabs the list of links inside the navbar(home, approve post, apply)
+//& it's looking for an element with the class .nvabar__menu.
+
+menu.addEventListener('click', function() { //It's saying that "when someone clicks the hamburger menu".
+    menu.classList.toggle('is-active'); //It adds or removes a CSS class to show or hide the menu links.
+    menuLinks.classList.toggle('active');//toggle just means:
+//If the class is there, take it off.
+//If the class is not there, put it on.
 });
